0a7d9ac9ef2ae54903652521bcd543e8999df58a
Joao Correia <joaope@gmail.com>
2016-05-09 23:30:54 +0100

    Added first meaningful tests

---
+++ src/CamlBuilder.UnitTests/SimpleOperatorTests.cs
@@ -0,0 +1,36 @@
+﻿namespace CamlBuilder.UnitTests
+{
+    using Xunit;
+    using System.Xml;
+
+    public class SimpleOperatorTests
+    {
+        [Fact]
+        public void VerifySimpleOperatorIsNullformat()
+        {
+            var op = CamlOperator.IsNull("testField");
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(op.GetCaml());
+
+            Assert.Equal(xmlDoc.FirstChild.Name, "IsNull");
+            Assert.Equal(xmlDoc.ChildNodes.Count, 1);
+            Assert.Equal(xmlDoc.ChildNodes[0].ChildNodes.Count, 1);
+            Assert.True(xmlDoc.SelectSingleNode("/IsNull/FieldRef[@Name='testField']") != null);
+        }
+
+        [Fact]
+        public void VerifySimpleOperatorIsNotNullFormat()
+        {
+            var op = CamlOperator.IsNotNull("testField");
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(op.GetCaml());
+
+            Assert.Equal(xmlDoc.FirstChild.Name, "IsNotNull");
+            Assert.Equal(xmlDoc.ChildNodes.Count, 1);
+            Assert.Equal(xmlDoc.ChildNodes[0].ChildNodes.Count, 1);
+            Assert.True(xmlDoc.SelectSingleNode("/IsNotNull/FieldRef[@Name='testField']") != null);
+        }
+    }
+}
